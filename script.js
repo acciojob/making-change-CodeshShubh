88c2be35d730a6557853d09467e6b017398291da
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-08 07:23:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const makeChange = (c) => {
   // your name here
+	let coinCounts = {
+  q: 0, // quarters
+  d: 0, // dimes
+  n: 0, // nickels
+  p: 0  // pennies
+};
+ // Start with the largest coin (quarters)
+  coinCounts.q = Math.floor(c / 25);
+  c %= 25; 
+  // Next, calculate the number of dimes
+  coinCounts.d = Math.floor(c / 10); 
+  c %= 10; 
+  // Next, calculate the number of nickels
+  coinCounts.n = Math.floor(c / 5); 
+  c %= 5; 
+  // Finally, calculate the number of pennies
+  coinCounts.p = c; 
+
+  return coinCounts; 
+
+	
 };
 
 // Do not the change the code below
